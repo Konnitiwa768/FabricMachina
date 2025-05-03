1d1d06bac3bcec615165d0af993e0c649f5cef66
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-03 11:06:49 +0900

    Create ImplementedInventory.java

---
+++ src/main/java/com/sakalti/fabricmachina/block/ImplementedInventory.java
@@ -0,0 +1,71 @@
+package com.sakalti.fabricmachina.block;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+
+public interface ImplementedInventory extends Inventory {
+    DefaultedList<ItemStack> getItems();
+
+    static ImplementedInventory of(DefaultedList<ItemStack> items) {
+        return () -> items;
+    }
+
+    static ImplementedInventory ofSize(int size) {
+        return of(DefaultedList.ofSize(size, ItemStack.EMPTY));
+    }
+
+    @Override
+    default int size() {
+        return getItems().size();
+    }
+
+    @Override
+    default boolean isEmpty() {
+        for (ItemStack stack : getItems()) {
+            if (!stack.isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    default ItemStack getStack(int slot) {
+        return getItems().get(slot);
+    }
+
+    @Override
+    default ItemStack removeStack(int slot, int count) {
+        ItemStack result = ItemStack.EMPTY;
+        ItemStack stack = getStack(slot);
+        if (!stack.isEmpty()) {
+            result = stack.split(count);
+            if (stack.isEmpty()) setStack(slot, ItemStack.EMPTY);
+        }
+        return result;
+    }
+
+    @Override
+    default ItemStack removeStack(int slot) {
+        return getItems().set(slot, ItemStack.EMPTY);
+    }
+
+    @Override
+    default void setStack(int slot, ItemStack stack) {
+        getItems().set(slot, stack);
+        if (stack.getCount() > getMaxCountPerStack()) {
+            stack.setCount(getMaxCountPerStack());
+        }
+    }
+
+    @Override
+    default void clear() {
+        getItems().clear();
+    }
+
+    @Override
+    default boolean canPlayerUse(PlayerEntity player) {
+        return true;
+    }
+}
